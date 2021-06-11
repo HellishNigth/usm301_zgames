c4bcd11d96c952123fbed365e403c4459c11a624
Ignacio <kritka200@gmail.com>
2021-06-10 20:19:50 -0400

    generar interfas de ver consolas y agregar consolas

--- web/zgames/resources/views/ver_consolas.blade.php
+++ web/zgames/resources/views/ver_consolas.blade.php
@@ -1,5 +1,21 @@
 @extends("layouts.master")
 
 @section("contenido")
-    <h1>hola soy ver consolas</h1>
+    <div class="row">
+        <div class="col-12 col-md-12 col-lg-6 mx-auto">
+            <table class="table table-hover table-bordered table-stripped table-responsive ">
+                <thead class="bg-info">
+                    <tr>
+                        <td>Nombre</td>
+                        <td>Marca</td>
+                        <td>Año de lanzamiento</td>
+                        <td>Acciones</td>
+                    </tr>
+                </thead>
+                <tbody id="tbody-consola">
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
