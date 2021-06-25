a8f1ba023a64cd4e497b62fe2dd86600d1f6c980
Ignacio <kritka200@gmail.com>
2021-06-24 20:21:56 -0400

    guardamos los valores de agregar consola

--- web/zgames/public/js/home.js
+++ web/zgames/public/js/home.js
@@ -1,4 +1,3 @@
-
 const cargarMarcas = async()=>{
     //1. Ir a buscar las marcas a la api
     let resultado = await axios.get("api/marcas/get");
@@ -13,3 +12,16 @@ const cargarMarcas = async()=>{
 };
 
 cargarMarcas();
+
+document.querySelector("#registrar-btn").addEventListener("click", async ()=>{
+    let nombre = document.querySelector("#nombre-txt").value;
+    let marca = document.querySelector("#marca-select").value;
+    let anio = document.querySelector("#anio-txt").value;
+    let consola = {};
+    consola.nombre = nombre;
+    consola.marca = marca;
+    consola.anio = anio;
+    console.log(consola);
+    let res = await crearConsola(consola);
+    Swal.fire("Consola creada","Consola creada existosamente","success");
+});
--- web/zgames/resources/views/layouts/master.blade.php
+++ web/zgames/resources/views/layouts/master.blade.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>Hello, world!</title>
+    <title>Zgames</title>
   </head>
     <body>
         <header>
@@ -26,7 +26,6 @@
                             <a class="nav-link" href="{{route('ver_consolas')}}">Ver Consolas</a>
                             <a class="nav-link" href="{{route('ver_juegos')}}">Ver Juegos</a>
                             <a class="nav-link" href="{{route('agregar_juego')}}">Agregar Juegos</a>
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </div>
                     </div>
                 </div>
@@ -36,7 +35,7 @@
             @yield('contenido')
         </main>
 
-
+        <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <script src="{{asset('js/axios_config.js')}}"></script>
